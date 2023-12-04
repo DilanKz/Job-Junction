dd520be1b8456ae5188b2a0ab9b682ab8465dbb9
Tharindu Dilan <Dilanmallawarachchi555@gmail.com>
2023-12-04 11:51:08 +0530

    added backend and front end modules

---
+++ Back_End/src/main/java/com/Job-Junction/Main.java
@@ -0,0 +1,11 @@
+package com.Job-Junction;
+
+/**
+ * @author Dilan
+ * @created ${DATE} - ${TIME}
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
