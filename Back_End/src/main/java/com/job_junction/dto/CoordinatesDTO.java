94815e76cba7cefcccc05e29a6e3abf2f1114c9e
Tharindu Dilan <Dilanmallawarachchi555@gmail.com>
2023-12-11 21:58:30 +0530

    Create CoordinatesDTO.java

---
+++ Back_End/src/main/java/com/job_junction/dto/CoordinatesDTO.java
@@ -0,0 +1,21 @@
+package com.job_junction.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * @author Dilan
+ * @created 11/12/2023 - 09:56 pm
+ */
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class CoordinatesDTO {
+
+    private double latitude;
+    private double longitude;
+
+}
+
