b3451fb3d8836db44a36247e14c481f550ee1e4d
Tharindu Dilan <Dilanmallawarachchi555@gmail.com>
2023-12-11 18:45:52 +0530

    Create JobPost.java

---
+++ Back_End/src/main/java/com/job_junction/model/JobPost.java
@@ -0,0 +1,35 @@
+package com.job_junction.model;
+
+/**
+ * @author Dilan
+ * @created 11/12/2023 - 06:43 pm
+ */
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.Date;
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Document(collection = "jobPosts")
+public class JobPost {
+
+    @Id
+    private String id;
+    private String companyId;
+    private String title;
+    private String description;
+    private List<String> skillsRequired;
+    private String salary;
+    private Date applicationDeadline;
+    private String category;
+    private int totalPositions;
+    //private List<Application> applications;
+
+}
+
