dc9af179f9209c44806820e5290f1c39d5501a3e
Tharindu Dilan <Dilanmallawarachchi555@gmail.com>
2023-12-11 18:41:19 +0530

    Create WebAppInitializer.java

---
+++ Back_End/src/main/java/com/job_junction/WebAppInitializer.java
@@ -0,0 +1,15 @@
+package com.job_junction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Dilan
+ * @created 11/12/2023 - 11:40 am
+ */
+@SpringBootApplication
+public class WebAppInitializer {
+    public static void main(String[] args) {
+        SpringApplication.run(WebAppInitializer.class);
+    }
+}
