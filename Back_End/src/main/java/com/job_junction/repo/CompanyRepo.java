10ac0ae6016de4c02b0f2b85240fee07de97b8a8
Tharindu Dilan <Dilanmallawarachchi555@gmail.com>
2023-12-11 21:25:48 +0530

    Create CompanyRepo.java

---
+++ Back_End/src/main/java/com/job_junction/repo/CompanyRepo.java
@@ -0,0 +1,14 @@
+package com.job_junction.repo;
+
+import com.job_junction.model.Company;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+/**
+ * @author Dilan
+ * @created 11/12/2023 - 09:21 pm
+ */
+
+public interface CompanyRepo extends MongoRepository<Company, String> {
+    // Add custom query methods if needed
+}
+
