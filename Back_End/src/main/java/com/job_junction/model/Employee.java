778b4abc0d14821cd0eb503855866c523ce87afb
Tharindu Dilan <Dilanmallawarachchi555@gmail.com>
2023-12-11 18:41:30 +0530

    Create Employee.java

---
+++ Back_End/src/main/java/com/job_junction/model/Employee.java
@@ -0,0 +1,32 @@
+package com.job_junction.model;
+
+/**
+ * @author Dilan
+ * @created 11/12/2023 - 06:36 pm
+ */
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Document(collection = "employees")
+public class Employee {
+
+    @Id
+    private String id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+    private List<String> skills;
+    private String profilePicture;
+    private String category;
+
+}
+
