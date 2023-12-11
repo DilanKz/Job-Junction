cfa4c618bfba67e6173f96c65ad006c2964de933
Tharindu Dilan <Dilanmallawarachchi555@gmail.com>
2023-12-11 18:41:40 +0530

    Create Coordinates.java

---
+++ Back_End/src/main/java/com/job_junction/model/Coordinates.java
@@ -0,0 +1,20 @@
+package com.job_junction.model;
+
+/**
+ * @author Dilan
+ * @created 11/12/2023 - 06:38 pm
+ */
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Coordinates {
+
+    private double latitude;
+    private double longitude;
+
+}
+
