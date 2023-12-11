7504e7470f09c57028ae7865b824fde3d6d88e30
Tharindu Dilan <Dilanmallawarachchi555@gmail.com>
2023-12-11 22:14:25 +0530

    Update JobPostService.java

--- Back_End/src/main/java/com/job_junction/service/JobPostService.java
+++ Back_End/src/main/java/com/job_junction/service/JobPostService.java
@@ -1,8 +1,19 @@
 package com.job_junction.service;
 
+import com.job_junction.dto.JobPostDTO;
+
+import java.util.List;
+
 /**
  * @author Dilan
  * @created 11/12/2023 - 10:10 pm
  */
 public interface JobPostService {
+    List<JobPostDTO> getAllJobPosts();
+
+    JobPostDTO getJobPostById(String id);
+
+    JobPostDTO saveJobPost(JobPostDTO jobPostDTO);
+
+    void deleteJobPost(String id);
 }
