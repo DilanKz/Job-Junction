e1ede7c3908e523431ec01dd64f1009f14c0616d
Tharindu Dilan <Dilanmallawarachchi555@gmail.com>
2023-12-11 22:06:39 +0530

    Update CompanyService.java

--- Back_End/src/main/java/com/job_junction/service/CompanyService.java
+++ Back_End/src/main/java/com/job_junction/service/CompanyService.java
@@ -1,8 +1,19 @@
 package com.job_junction.service;
 
+import com.job_junction.dto.CompanyDTO;
+
+import java.util.List;
+
 /**
  * @author Dilan
  * @created 11/12/2023 - 10:02 pm
  */
 public interface CompanyService {
+    List<CompanyDTO> getAllCompanies();
+
+    CompanyDTO getCompanyById(String id);
+
+    CompanyDTO saveCompany(CompanyDTO companyDTO);
+
+    void deleteCompany(String id);
 }
