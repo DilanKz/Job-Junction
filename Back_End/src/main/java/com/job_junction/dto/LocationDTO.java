6ab0e8a17e5640e6ef7259db44df9874f9f31770
Tharindu Dilan <Dilanmallawarachchi555@gmail.com>
2023-12-11 21:58:35 +0530

    Create LocationDTO.java

---
+++ Back_End/src/main/java/com/job_junction/dto/LocationDTO.java
@@ -0,0 +1,22 @@
+package com.job_junction.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * @author Dilan
+ * @created 11/12/2023 - 09:55 pm
+ */
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class LocationDTO {
+
+    private String city;
+    private String state;
+    private String country;
+    private CoordinatesDTO coordinates;
+}
+
