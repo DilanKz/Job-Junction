f2e4ac79e31eb475f1838e5ded2caf010492bdd1
Tharindu Dilan <Dilanmallawarachchi555@gmail.com>
2023-12-12 15:54:33 +0530

    Create JobPostController.java

---
+++ Back_End/src/main/java/com/job_junction/controller/JobPostController.java
@@ -0,0 +1,42 @@
+package com.job_junction.controller;
+
+import com.job_junction.dto.JobPostDTO;
+import com.job_junction.service.JobPostService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+
+/**
+ * @author Dilan
+ * @created 12/12/2023 - 03:49 pm
+ */
+
+
+@RestController
+@RequestMapping("/job-posts")
+public class JobPostController {
+
+    @Autowired
+    JobPostService jobPostService;
+
+    @GetMapping
+    public List<JobPostDTO> getAllJobPosts() {
+        return jobPostService.getAllJobPosts();
+    }
+
+    @GetMapping("/{id}")
+    public JobPostDTO getJobPostById(@PathVariable String id) {
+        return jobPostService.getJobPostById(id);
+    }
+
+    @PostMapping
+    public JobPostDTO saveJobPost(@RequestBody JobPostDTO jobPostDTO) {
+        return jobPostService.saveJobPost(jobPostDTO);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteJobPost(@PathVariable String id) {
+        jobPostService.deleteJobPost(id);
+    }
+}
+
