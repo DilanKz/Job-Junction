fac5f65b056ed38441add96c2e108f697ebb8239
Tharindu Dilan <Dilanmallawarachchi555@gmail.com>
2023-12-13 20:10:20 +0530

    Create UserController.java

---
+++ Back_End/src/main/java/com/job_junction/controller/UserController.java
@@ -0,0 +1,32 @@
+package com.job_junction.controller;
+
+/**
+ * @author Dilan
+ * @created 13/12/2023 - 08:08 pm
+ */
+import com.job_junction.dto.UserDTO;
+import com.job_junction.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    @Autowired
+    UserService userService;
+
+
+    @GetMapping
+    public List<UserDTO> getAllUsers() {
+        return userService.getAllUsers();
+    }
+
+    @PostMapping("/register")
+    public UserDTO registerUser(@RequestBody UserDTO userDTO) {
+        userDTO.setPassword((userDTO.getPassword()));
+        return userService.saveUser(userDTO);
+    }
+}
+
