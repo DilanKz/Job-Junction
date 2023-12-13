c89737e5bb8dcadf679a4cf75e981c53b0e5bd91
Tharindu Dilan <Dilanmallawarachchi555@gmail.com>
2023-12-13 19:28:19 +0530

    Create UserService.java

---
+++ Back_End/src/main/java/com/job_junction/service/UserService.java
@@ -0,0 +1,21 @@
+package com.job_junction.service;
+
+import com.job_junction.dto.UserDTO;
+
+import java.util.List;
+
+/**
+ * @author Dilan
+ * @created 13/12/2023 - 07:17 pm
+ */
+public interface UserService {
+    List<UserDTO> getAllUsers();
+
+    UserDTO getUserById(String id);
+
+    UserDTO getUserByUsername(String username);
+
+    UserDTO saveUser(UserDTO userDTO);
+
+    void deleteUser(String id);
+}
