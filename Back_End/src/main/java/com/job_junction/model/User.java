790d1207fe0b7168ce4083e2e3aa019291185077
Tharindu Dilan <Dilanmallawarachchi555@gmail.com>
2023-12-13 20:53:55 +0530

    Update User.java

--- Back_End/src/main/java/com/job_junction/model/User.java
+++ Back_End/src/main/java/com/job_junction/model/User.java
@@ -1,5 +1,8 @@
 package com.job_junction.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 import java.util.List;
@@ -9,6 +12,9 @@ import java.util.List;
  * @created 13/12/2023 - 07:06 pm
  */
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 @Document(collection = "users")
 public class User {
 
