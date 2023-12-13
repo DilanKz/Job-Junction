e678dfaa95eafae47b0747144220e567ca20a3c3
Tharindu Dilan <Dilanmallawarachchi555@gmail.com>
2023-12-13 19:01:47 +0530

    Create UserDTO.java

---
+++ Back_End/src/main/java/com/job_junction/dto/UserDTO.java
@@ -0,0 +1,25 @@
+package com.job_junction.dto;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+/**
+ * @author Dilan
+ * @created 13/12/2023 - 07:00 pm
+ */
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserDTO {
+
+    private String id;
+    private String username;
+    private String password;
+    private List<String> roles;
+}
+
