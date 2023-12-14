615375872b399ac8b7421b55aecc8b4f9fe21e1a
Tharindu Dilan <Dilanmallawarachchi555@gmail.com>
2023-12-14 19:02:26 +0530

    Create loginForm.js

---
+++ Front_End/forms/loginForm.js
@@ -0,0 +1,27 @@
+function userData() {
+    return {
+        id:"",
+        username:"",
+        password:"",
+        entityid:""
+    }
+}
+
+function employeeData() {
+    return {
+        id:"",
+        name:"",
+        industry:"",
+        email:""
+    }
+}
+
+function companyData() {
+    return {
+        id:"",
+        name:"",
+        industry:"",
+        email:"",
+        state:""
+    }
+}
