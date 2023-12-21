46c76a3c7166d4e1201d405742fd9f22c8768982
Tharindu Dilan <Dilanmallawarachchi555@gmail.com>
2023-12-21 13:59:23 +0530

    update controllers

--- Back_End/src/main/java/com/job_junction/controller/EmployeeController.java
+++ Back_End/src/main/java/com/job_junction/controller/EmployeeController.java
@@ -13,6 +13,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/employees")
+@CrossOrigin
 public class EmployeeController {
 
     @Autowired
--- Back_End/src/main/java/com/job_junction/controller/JobPostController.java
+++ Back_End/src/main/java/com/job_junction/controller/JobPostController.java
@@ -14,6 +14,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/job-posts")
+@CrossOrigin
 public class JobPostController {
 
     @Autowired
