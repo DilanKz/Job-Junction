610704f1d95e033ebf4ce139b016284b66052dd0
Tharindu Dilan <Dilanmallawarachchi555@gmail.com>
2023-12-22 21:36:19 +0530

    Update companyPostController.js

---
+++ Front_End/controllers/companyPostController.js
@@ -0,0 +1,26 @@
+$('#btnAddPost').click(function() {
+    const allFieldsValid = validateAllFields();
+
+    if (allFieldsValid) {
+        addPost()
+    } else {
+        toastShower('1','bg-danger','text-light','Invalid Data');
+    }
+});
+
+function addPost(post) {
+
+    $.ajax({
+        url:baseURL+'job-posts/save',
+        data:JSON.stringify(post),
+        method:'POST',
+        contentType: 'application/json',
+        success:function (response) {
+
+        },
+        error:function (e) {
+            toastShower('1','bg-danger','text-light','Try again');
+        }
+    });
+
+}
