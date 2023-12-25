0193c31a59ac7865a30f54e5a3f512474f3f1ebb
Tharindu Dilan <Dilanmallawarachchi555@gmail.com>
2023-12-25 20:15:24 +0530

    Update JobPostDTO.java

--- Back_End/src/main/java/com/job_junction/dto/JobPostDTO.java
+++ Back_End/src/main/java/com/job_junction/dto/JobPostDTO.java
@@ -19,6 +19,8 @@ public class JobPostDTO {
     private String id;
     private CompanyDTO companyId;
     private String title;
+    private String industry;
+    private String type;
     private String description;
     private List<String> skillsRequired;
     private String salary;
