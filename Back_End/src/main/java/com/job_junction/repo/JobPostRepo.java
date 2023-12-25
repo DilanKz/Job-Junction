1146e066b22484fe06d10722617ded63f593eba8
Tharindu Dilan <Dilanmallawarachchi555@gmail.com>
2023-12-25 20:17:48 +0530

    Update JobPostRepo.java

--- Back_End/src/main/java/com/job_junction/repo/JobPostRepo.java
+++ Back_End/src/main/java/com/job_junction/repo/JobPostRepo.java
@@ -3,6 +3,8 @@ package com.job_junction.repo;
 import com.job_junction.model.JobPost;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.util.List;
+
 /**
  * @author Dilan
  * @created 11/12/2023 - 09:21 pm
@@ -10,5 +12,7 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 
 public interface JobPostRepo extends MongoRepository<JobPost, String> {
     JobPost findTopByOrderByIdDesc();
+
+    List<JobPost> findByCompanyId_Id(String companyId);
 }
 
