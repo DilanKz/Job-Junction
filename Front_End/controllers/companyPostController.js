de16761d06a35cb2dce719729d1743851ab553b0
Tharindu Dilan <Dilanmallawarachchi555@gmail.com>
2023-12-26 18:02:59 +0530

    Update companyPostController.js

--- Front_End/controllers/companyPostController.js
+++ Front_End/controllers/companyPostController.js
@@ -3,6 +3,7 @@ let selectedPost = {};
 let addPost = $('#btnAddPost');
 let deletePost = $('#btnDeletePost');
 let updatePost = $('#btnUpdatePost');
+let refreshPost = $('#btnRefreshPost');
 
 
 addPost.click(function () {
@@ -37,10 +38,10 @@ function savePost(post) {
 
 
 $('#link-DB').click(function () {
-    getPosted()
-})
+    getPosts()
+});
 
-function getPosted() {
+function getPosts() {
 
     $.ajax({
         url: baseURL + 'job-posts/posted?id=' + userAccount.company.id,
@@ -83,10 +84,67 @@ $(document).on('click', '.post-tr', function () {
 
     let parseOB = JSON.parse($(this).attr('post'));
     console.log(parseOB);
+
+    selectedPost = parseOB;
+
     $('#txtJobTitle').val(parseOB.title)
     $('#txtJobDescription').val(parseOB.description)
     $('#txtSalary').val(parseOB.salary)
     $('#txtSelectType').val(parseOB.type)
     $('#txtSelectIndustry').val(parseOB.industry)
 
+});
+
+updatePost.click(function () {
+
+    selectedPost.title = $('#txtJobTitle').val()
+    selectedPost.description = $('#txtJobDescription').val()
+    selectedPost.salary = $('#txtSalary').val()
+    selectedPost.type = $('#txtSelectType').val()
+    selectedPost.industry = $('#txtSelectIndustry').val()
+
+    $.ajax({
+        url: baseURL + 'job-posts/save',
+        data: JSON.stringify(selectedPost),
+        method: 'POST',
+        contentType: 'application/json',
+        success: function (response) {
+            toastShower('1', 'bg-success', 'text-light', 'Post is successfully updated');
+            clearPostFields();
+            selectedPost = {}
+            deletePost.toggleClass('d-none')
+            updatePost.toggleClass('d-none')
+            addPost.toggleClass('d-none')
+        },
+        error: function (e) {
+            toastShower('1', 'bg-danger', 'text-light', 'Error try again');
+        }
+    });
+
+});
+
+deletePost.click(function () {
+    $.ajax({
+        url: baseURL + 'job-posts?id=' + selectedPost.id,
+        method: 'DELETE',
+        contentType: 'application/json',
+        success: function (response) {
+            toastShower('1', 'bg-success', 'text-light', 'Post is successfully updated');
+            clearPostFields();
+            selectedPost = {}
+            deletePost.toggleClass('d-none')
+            updatePost.toggleClass('d-none')
+            addPost.toggleClass('d-none')
+        },
+        error: function (e) {
+            toastShower('1', 'bg-danger', 'text-light', 'Error try again');
+        }
+    });
+});
+
+refreshPost.click(function () {
+    clearPostFields()
+    deletePost.addClass('d-none')
+    updatePost.addClass('d-none')
+    addPost.removeClass('d-none')
 });
