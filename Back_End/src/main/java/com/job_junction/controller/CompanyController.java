f52763e79c13c2731ca9045892db68e6052dcca4
Tharindu Dilan <Dilanmallawarachchi555@gmail.com>
2023-12-31 22:19:36 +0530

    Update CompanyController.java

--- Back_End/src/main/java/com/job_junction/controller/CompanyController.java
+++ Back_End/src/main/java/com/job_junction/controller/CompanyController.java
@@ -1,9 +1,15 @@
 package com.job_junction.controller;
 
 import com.job_junction.dto.CompanyDTO;
+import com.job_junction.dto.LocationDTO;
+import com.job_junction.dto.UserDTO;
 import com.job_junction.service.CompanyService;
+import com.job_junction.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
 import java.util.List;
 
 /**
@@ -18,6 +24,9 @@ public class CompanyController {
 
     @Autowired
     CompanyService companyService;
+
+    @Autowired
+    UserService userService;
 
 
     @GetMapping("/getAll")
@@ -38,6 +47,32 @@ public class CompanyController {
     @DeleteMapping("/{id}")
     public void deleteCompany(@PathVariable String id) {
         companyService.deleteCompany(id);
+    }
+
+    @PostMapping("/pfp")
+    public UserDTO addProfilePicture(@RequestParam("image") MultipartFile file, @RequestParam("id") String id ,@RequestParam("uid") String uid) throws IOException {
+        CompanyDTO companyById = companyService.getCompanyById(id);
+        companyById.setProfilePicture(file.getBytes());
+
+        CompanyDTO company = companyService.saveCompany(companyById);
+
+        UserDTO userById = userService.getUserById(uid);
+        userById.setCompany(company);
+
+        return userService.saveUser(userById);
+    }
+
+    @PostMapping("/location")
+    public UserDTO addLocation(@RequestBody LocationDTO dto , @RequestParam("id") String id ,@RequestParam("uid") String uid){
+        CompanyDTO companyById = companyService.getCompanyById(id);
+        companyById.setLocation(dto);
+
+        CompanyDTO company = companyService.saveCompany(companyById);
+
+        UserDTO userById = userService.getUserById(uid);
+        userById.setCompany(company);
+
+        return userService.saveUser(userById);
     }
 }
 
