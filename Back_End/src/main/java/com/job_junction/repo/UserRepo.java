3ebea1c0c607390ef8d288085e8aa2fc7bd57b5f
Tharindu Dilan <Dilanmallawarachchi555@gmail.com>
2024-01-03 21:50:02 +0530

    Update UserRepo.java

--- Back_End/src/main/java/com/job_junction/repo/UserRepo.java
+++ Back_End/src/main/java/com/job_junction/repo/UserRepo.java
@@ -2,6 +2,8 @@ package com.job_junction.repo;
 
 import com.job_junction.model.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
+
 import java.util.Optional;
 
 
@@ -14,5 +16,7 @@ public interface UserRepo extends MongoRepository<User, String> {
 
     Optional<User> findByUsername(String username);
     User findTopByOrderByIdDesc();
+    @Query("{'id': ?0}")
+    void updatePasswordById(String userId, String newPassword);
 }
 
