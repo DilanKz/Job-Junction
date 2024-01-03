0f811bad902b498065506cb78233e9b172e595e3
Tharindu Dilan <Dilanmallawarachchi555@gmail.com>
2024-01-03 20:55:05 +0530

    Update loginController.js

--- Front_End/controllers/loginController.js
+++ Front_End/controllers/loginController.js
@@ -1,4 +1,4 @@
-$("#btnCustomerReg").click(function() {
+$("#btnCustomerReg").click(function () {
     let selectedRadioButton = $("input[name='options-outlined']:checked");
 
     if (validateRegisterFields()) {
@@ -10,39 +10,39 @@ $("#btnCustomerReg").click(function() {
             if (selectedText === 'Employee') {
 
                 let employee = employeeData();
-                employee.id='';
-                employee.name=$('#txtName').val();
-                employee.email=$('#txtEmail').val();
-                employee.industry= $("#txtIndustry").val();
+                employee.id = '';
+                employee.name = $('#txtName').val();
+                employee.email = $('#txtEmail').val();
+                employee.industry = $("#txtIndustry").val();
 
 
                 let data = userData();
-                data.id='';
-                data.username=$('#txtUsername').val();
-                data.password=$('#txtPass').val();
-                data.type='employees';
-                data.entityid='';
+                data.id = '';
+                data.username = $('#txtUsername').val();
+                data.password = $('#txtPass').val();
+                data.type = 'employees';
+                data.entityid = '';
 
-                makeAccount('employees',data,employee);
+                makeAccount('employees', data, employee);
 
-            }else if (selectedText==='Company'){
+            } else if (selectedText === 'Company') {
 
                 let company = companyData();
-                company.id='';
-                company.name=$('#txtName').val();
-                company.email=$('#txtEmail').val();
-                company.industry= $("#txtIndustry").val();
-                company.state= 'new';
+                company.id = '';
+                company.name = $('#txtName').val();
+                company.email = $('#txtEmail').val();
+                company.industry = $("#txtIndustry").val();
+                company.state = 'new';
 
 
                 let data = userData();
-                data.id='';
-                data.username=$('#txtUsername').val();
-                data.password=$('#txtPass').val();
-                data.type='companies';
-                data.entityid='';
+                data.id = '';
+                data.username = $('#txtUsername').val();
+                data.password = $('#txtPass').val();
+                data.type = 'companies';
+                data.entityid = '';
 
-                makeAccount('companies',data,company);
+                makeAccount('companies', data, company);
             }
 
 
@@ -50,76 +50,73 @@ $("#btnCustomerReg").click(function() {
             console.log("No radio button selected.");
         }
 
-    }else {
+    } else {
         console.log("invalid")
     }
 });
-$("#btnLogin").click(function() {
+$("#btnLogin").click(function () {
     let username = $('#txtUN').val();
     let password = $('#txtPW').val();
 
-    loginIn(username,password);
+    loginIn(username, password);
 
 });
 
 
-
-
-function makeAccount(url,user,account) {
+function makeAccount(url, user, account) {
     $.ajax({
-        url:baseURL+url+'/save',
-        data:JSON.stringify(account),
-        method:'POST',
+        url: baseURL + url + '/save',
+        data: JSON.stringify(account),
+        method: 'POST',
         contentType: 'application/json',
-        success:function (response) {
-            if (url==='employees'){
-                user.employee=response;
-                user.company=null;
-            }else if (url==='companies'){
-                user.company=response;
-                user.employee=null;
+        success: function (response) {
+            if (url === 'employees') {
+                user.employee = response;
+                user.company = null;
+            } else if (url === 'companies') {
+                user.company = response;
+                user.employee = null;
             }
 
             $.ajax({
-                url:baseURL+'users/register',
-                data:JSON.stringify(user),
-                method:'POST',
+                url: baseURL + 'users/register',
+                data: JSON.stringify(user),
+                method: 'POST',
                 contentType: 'application/json',
-                success:function (response) {
+                success: function (response) {
                     console.log(response);
                     clearFields();
-                    toastShower('1','bg-success','text-light','successfully registered');
+                    toastShower('1', 'bg-success', 'text-light', 'successfully registered');
                 },
-                error:function (e) {
-                    toastShower('1','bg-danger','text-light','Try again');
+                error: function (e) {
+                    toastShower('1', 'bg-danger', 'text-light', 'Try again');
                 }
             });
         },
-        error:function (e) {
-            toastShower('1','bg-danger','text-light','Try again');
+        error: function (e) {
+            toastShower('1', 'bg-danger', 'text-light', 'Try again');
         }
     });
 }
 
 
-
-function loginIn(user,password) {
+function loginIn(user, password) {
 
     $.ajax({
-        url:baseURL+'users/login?user='+user,
+        url: baseURL + 'users/login?user=' + user,
         contentType: 'application/json',
         // async:false,
-        success:function (response) {
+        success: function (response) {
 
-            if (response){
+            if (response) {
                 console.log()
                 if (response.password === password) {
 
-                    userAccount.user=response
+                    userAccount.user = response
 
-                    if (response.type==='companies'){
-                        userAccount.company=response.company;
-                        userAccount.employee="";
+                    if (response.type === 'companies') {
+                        userAccount.company = response.company;
+                        userAccount.employee = "";
 
                         /*const newMarkerPosition = {
                             lat: parseFloat(response.company.location.coordinates.latitude),
@@ -128,7 +125,7 @@ function loginIn(user,password) {
                         addInfoWindow(newMarkerPosition,byteArrayToImage(response.company.profilePicture));*/
 
                         $('#btn-login-main').toggleClass('d-none');
-                        let button=`<button id="btn-profile" class="btn btn-sm btn-primary me-3">
+                        let button = `<button id="btn-profile" class="btn btn-sm btn-primary me-3">
                                     ${response.username}
                                     <i class="bi bi-person-fill ps-2"></i>
                                 </button>`
@@ -137,17 +134,17 @@ function loginIn(user,password) {
                         $('#link-DB').removeClass('d-none');
                         $('#link-Pro').addClass('d-none');
 
-                        setDataToDashboard(userAccount.company.profilePicture,userAccount.company.name);
+                        setDataToDashboard(userAccount.company.profilePicture, userAccount.company.name);
 
-                    }else if (response.type==='employees'){
-                        userAccount.employee=response.employee;
-                        userAccount.company="";
+                    } else if (response.type === 'employees') {
+                        userAccount.employee = response.employee;
+                        userAccount.company = "";
 
                         $('#link-Pro').removeClass('d-none');
                         $('#link-DB').addClass('d-none');
 
                         $('#btn-login-main').toggleClass('d-none');
-                        let button=`<button id="btn-profile" class="btn btn-sm btn-success me-3">
+                        let button = `<button id="btn-profile" class="btn btn-sm btn-success me-3">
                                     ${response.username}
                                     <i class="bi bi-person-fill ps-2"></i>
                                 </button>`
@@ -162,28 +159,66 @@ function loginIn(user,password) {
                     $('#txtUN').val("");
                     $('#txtPW').val("");
 
-                    toastShower('1','bg-success','text-light','successfully logged in');
+                    toastShower('1', 'bg-success', 'text-light', 'successfully logged in');
 
-                }else {
-                    toastShower('1','bg-danger','text-light','Wrong Password try again');
+                } else {
+                    toastShower('1', 'bg-danger', 'text-light', 'Wrong Password try again');
                 }
-            }else {
-                toastShower('1','bg-danger','text-light','No Account found');
+            } else {
+                toastShower('1', 'bg-danger', 'text-light', 'No Account found');
             }
 
             console.log(response);
         },
-        error:function (e) {
-            toastShower('1','bg-danger','text-light','Try again');
+        error: function (e) {
+            toastShower('1', 'bg-danger', 'text-light', 'Try again');
         }
     });
 
 }
 
 function setDataToDashboard() {
-    $('#imgPfp').prop('src',byteArrayToImage(userAccount.company.profilePicture))
+    $('#imgPfp').prop('src', byteArrayToImage(userAccount.company.profilePicture))
     $('#lblCompName').text(userAccount.company.name);
 
-    $('#lblPfpCompany').css('background-image', 'url(' +byteArrayToImage(userAccount.company.profilePicture)+ ')');
+    $('#lblPfpCompany').css('background-image', 'url(' + byteArrayToImage(userAccount.company.profilePicture) + ')');
     $('#txtAddress').val(userAccount.company.location.city);
 }
+
+/*Change password*/
+let otp;
+$("#txtLoginPOTP").click(function () {
+    let username = $("#txtLoginName").val();
+
+    if (username.length < 3) {
+        $("#txtLoginName").css('border', '2px solid red');
+    } else {
+        //Send the otp
+        $.ajax({
+            url: baseURL + 'users/reset?user=' + username,
+            contentType: 'application/json',
+            async: false,
+            success: function (response) {
+
+            },
+            error: function (error) {
+            }
+
+        });
+    }
+
+});
+
+$("#txtLoginName").click(function () {
+    $("#txtLoginName").css('border', '1px solid lightgray');
+});
+
+$('#btnChangePass').click(function () {
+
+    if (validateField($("#txtLoginPassword").val(), passwordRegex)) {
+        //user change pass
+
+
+    }
+
+});
